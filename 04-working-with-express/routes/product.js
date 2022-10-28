fb174f9d0b9097b40ed73efd7e76f52c606994a8
szalai-istvan <80052683+szalai-istvan@users.noreply.github.com>
2022-10-28 16:34:22 +0200

    wrap-up of section 4

--- 04-working-with-express/routes/product.js
+++ 04-working-with-express/routes/product.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
@@ -5,7 +6,7 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded());
 
 router.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
