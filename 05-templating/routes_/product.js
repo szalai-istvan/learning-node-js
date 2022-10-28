7796f5b91b5058dcb0d922ae4c1c2bd1b0c9cf65
szalai-istvan <80052683+szalai-istvan@users.noreply.github.com>
2022-10-28 18:41:10 +0200

    Templating, first commit

---
+++ 05-templating/routes_/product.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+router.use(bodyParser.urlencoded());
+
+router.use('/add-product', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'views_', 'add-product.html'));
+});
+
+router.post('/product', (req, res, next) => {
+    fs.appendFileSync('log.txt', req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
